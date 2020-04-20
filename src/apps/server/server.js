112ff5deada283253442460e28feb9648a7e4030
vladimirkr <vladimir.kravchenko@playtech.com>
2020-04-20 05:43:05 +0300

    Feature: Simple http server with own emit

--- src/apps/server/server.js
+++ src/apps/server/server.js
@@ -2,6 +2,12 @@ const http = require('http');
 
 const server = new http.Server();
 server.listen(2000, 'localhost');
+let emit = server.emit;
+
+server.emit = function(event) {
+  console.log(event);
+  emit.apply(server, arguments);
+}
 
 let counter = 0;
 server.on('request', function(req, res) {
