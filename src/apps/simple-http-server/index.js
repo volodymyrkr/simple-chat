20917a78d51e99edb228bf066ccd131fada048eb
volodymyrkr <volodymyr.keavchenko.2000@gmail.com>
2020-05-10 10:57:50 +0300

    Feature: Added html,script,favicon loading handlers

--- src/apps/simple-http-server/index.js
+++ src/apps/simple-http-server/index.js
@@ -33,7 +33,34 @@ function httpRequestHandler(req, res) {
 
   console.log(`Request name: ${method}`);
   console.log(parsedUrl.query);
-  res.end('Hello from server');
+  switch(method) {
+    case '/index.html':
+      res.writeHead(200, { 'Content-type': 'text/html'})
+      res.end(`
+        <!doctype>
+        <html>
+          <body>
+            <div>Hello from server </div>
+            <button class="button">Ok</button>
+            <script src="/app.js"></script>
+          </body>
+        </html>
+      `);
+      break;
+    case '/app.js':
+      res.writeHead(200, { 'Content-type': 'text/javascript'})
+      res.end(`
+        var button = document.querySelector('button');
+        button.addEventListener('click', function() {
+          alert("Button is clicked!!!");
+          console.log('Button is clicked!!!');
+        });
+      `)
+      break;
+    default:
+      res.statusCode = 404;
+  }
+  res.end();
 }
 
 function closeHandler() {
