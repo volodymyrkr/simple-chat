c6f7dddc818e10652d8d3b31029af8dda4447050
volodymyrkr <volodymyr.keavchenko.2000@gmail.com>
2020-05-30 19:21:56 +0300

    Improvement: errors handling

--- src/apps/login-http-server/build/index.js
+++ src/apps/login-http-server/build/index.js
@@ -4,20 +4,40 @@ function getUsersHandler() {
     .then(result => { // convert parse
       // d response to html
       document.getElementById('usersList').innerHTML =
-        '<ul>' + result.map(
-          user => `<li>${user.name}</li>`
+        '<ul>' + result.reverse().map(
+        user => `<li>${user.name}</li>`
         ).join("") +
         '</ul>'
     });
 
 }
 
-function addNewUserClick() {
-  fetch(`/add?user=${document.getElementById('newUserName').value}`)
-    .then(response => response.json())
-    .then((response) => {
-      if (response.result === 200) {
+function clearUserListHandler() {
+  fetch('/clear')
+    .then( response => response.json()) // get parsed response in promise
+    .then( response => handleErrors(response)) // handle clear user list errors
+    .then( response => {
+      if (response.code === 200) {
         getUsersHandler();
       }
     });
+
+}
+
+function addNewUserClick() {
+  fetch(`/add?user=${document.getElementById('userName').value}&pass=${document.getElementById('userPass').value}`)
+    .then( response => response.json())
+    .then( response => handleErrors(response)) // handle clear user list errors
+    .then( response => {
+      if (response.code === 200) {
+        getUsersHandler();
+      }
+    });
+}
+
+function handleErrors(response) {
+  if (response.code !== 200) {
+    console.error(`Error code: ${response.result} result: ${JSON.stringify(response)}`);
+  }
+  return response;
 }
--- src/apps/login-http-server/index.js
+++ src/apps/login-http-server/index.js
@@ -3,7 +3,7 @@ const log = require('../../utils/log');
 const express = require('express');
 
 const server = express();
-const users = [
+let users = [
   {name: "Vladimir", password: "123456"},
 ];
 
@@ -20,17 +20,56 @@ server.get('/users', (req, res, next) => {
 
 server.get('/add', (req, res, next) => {
   console.log(req.query)
-  users.push({
-    name: req.query.user,
-    password: 'nopass'
-  })
+  const result = validateAddUserRequest(req.query);
+  if (result.code === 200) {
+    users.push({
+      name: req.query.user,
+      password: req.query.pass,
+    });
+  }
   res.writeHead(200, {"Content-type": "text.html"});
   res.end(JSON.stringify({
-      result: 200,
+      ...result,
       ...req.query,
-      ...users,
-  }
+    }
   ));
+});
+
+function validateAddUserRequest(query) {
+  if (!query.user) {
+    return {
+      code: 401,
+      message: "user name is missed"
+    }; // no username
+  }
+  if (!query.pass) {
+    return {
+      code: 402,
+      message: "password name is missed"
+    }; // no pass
+  }
+  if (userRegistered(query.user)) {
+    return {
+      code: 403,
+      message: "user is registered"
+    }; // user registered
+  }
+  return {
+    code: 200,
+    message: "user is added"
+  };
+}
+
+function userRegistered(userName) {
+  return users.some(({name}) => name === userName);
+}
+server.get('/clear', (req, res,  next) => {
+  users = [];
+  res.end(
+    JSON.stringify({
+      code: 200
+    })
+  )
 });
 
 server.listen(3008);
