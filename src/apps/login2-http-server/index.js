862186a0c793071969ae9739b162c3d8688f6cae
volodymyrkr <volodymyr.keavchenko.2000@gmail.com>
2020-05-31 14:20:11 +0300

    Feature: register form is added

--- src/apps/login2-http-server/index.js
+++ src/apps/login2-http-server/index.js
@@ -6,10 +6,12 @@ const server = express();
 server.use(express.static(__dirname +'/public/'));
 
 server.get('/login', (req, res, next) => {
+  console.log("Connection");
   res.render(__dirname +'/views/login.ejs');
 });
 
 server.post('/login', (req, res, next) => {
+  console.log("Log");
   res.end("Logged");
 });
 
@@ -18,7 +20,7 @@ server.get('/register', (req, res, next) => {
 });
 
 server.post('/register', (req, res, next) => {
-  res.end("Logged");
+  res.end("Registered");
 });
 
 
