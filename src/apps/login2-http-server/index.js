def450ac9d9ec2403b8525c27737ec414556bee1
volodymyrkr <volodymyr.keavchenko.2000@gmail.com>
2020-05-31 12:55:50 +0300

    Feature: Very simple static server with ejs views

--- src/apps/login2-http-server/index.js
+++ src/apps/login2-http-server/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 const server = express();
 
 server.use(express.static(__dirname +'/public/'));
 
-server.get('/', (req, res, next) => {
-  console.log('connection');
-})
+server.get('/login', (req, res, next) => {
+  res.render(__dirname +'/views/login.ejs');
+});
 
-server.listen(3009)
+server.post('/login', (req, res, next) => {
+  res.end("Logged");
+});
+
+server.get('/register', (req, res, next) => {
+  res.render(__dirname +'/views/register.ejs');
+});
+
+server.post('/register', (req, res, next) => {
+  res.end("Logged");
+});
+
+
+server.listen(3009);
